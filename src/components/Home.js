97a1d2ba1df6312cf3c26824332f3ccf3a0af359
Ines Montero <ines@minnekdigital.com>
2023-05-29 15:47:54 -0400

    apply styles for form

--- src/components/Home.js
+++ src/components/Home.js
@@ -110,6 +110,7 @@ const Home = () => {
         multiple
       />
       <Button onClick={handleFileClick} component="span" className="btn">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
         Seleccionar Archivos
       </Button>
 
@@ -124,6 +125,7 @@ const Home = () => {
 
       <FormControl component="fieldset" className="optionsContainer">
         <FormLabel component="legend" className="divider">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M3.9 54.9C10.5 40.9 24.5 32 40 32H472c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9V448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6V320.9L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z"/></svg>
           Opciones
         </FormLabel>
 
@@ -178,7 +180,10 @@ const Home = () => {
         </div>
       </div>
       <input type="submit" onClick={onSubmit} hidden />
-      <Button type="submit">Download</Button>
+      <Button type="submit" className="submitButton">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
+        Download
+      </Button>
     </form>
   );
 };
